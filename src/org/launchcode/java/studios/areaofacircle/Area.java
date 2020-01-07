8462ec0844b478f79dcee1d5a8c2bdc6af52b172
Alex <avmetzger@gmail.com>
2020-01-06 18:52:04 -0600

    added Circle.getArea method

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,4 +1,14 @@
 package org.launchcode.java.studios.areaofacircle;
 
+import java.util.Scanner;
+
+
 public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input the radius of a circle to calculate its area:");
+        Double radius = input.nextDouble();
+        Double area = Circle.getArea(radius);
+        System.out.println("The area of a circle of radius " + radius + " is: " + area);
+    }
 }
